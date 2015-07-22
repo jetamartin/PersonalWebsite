76b45bafc5da3338b40f43c803594f625d9c1107
Jet Martin <jetamartin@gmail.com>
2015-07-22 12:43:11 -0700

    added config change to /config/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+# Added to try and address ActionController::RoutingError (No route matches "/stylesheets/all.css"):
+  config.serve_static_assets = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
